3c89e5a1aeec9ebf53a8aa548b5067c67443aac9
prebollido <pbrebollido@up.edu.ph>
2026-03-18 09:50:14 +0800

    sync changes

--- cos-app/src/routes/+page.svelte
+++ cos-app/src/routes/+page.svelte
@@ -1,12 +1,17 @@
 <script>
   let username = "";
   let password = "";
+  import { goto } from '$app/navigation';
+
+    function log_in() {
+        goto('/generate_cert');
+    }
 </script>
 
-<section id="main" class="flex items-center justify-center min-h-screen">
-    <div class="flex flex-col items-center w-16 sm:w-25 md:w-50 lg:w-100 h-auto mx-auto">
-        <img src="/images/logo.svg" alt="logo" class="w-16 sm:w-25 md:w-50 lg:w-100 h-auto mx-auto"/>
-        <div id="log-in" class="py-5 px-2 w-100">
+<section id="main" class="flex flex-col items-center justify-center mt-45">
+    <img src="/images/logo.svg" alt="logo" class="w-16 sm:w-25 md:w-50 lg:w-80 h-auto mx-auto"/>
+    <div class="flex flex-col items-center w-16 sm:w-25 md:w-50 lg:w-80 h-auto mx-auto">
+        <div id="log-in" class="py-5 px-2 w-80">
             <h3 class="text-lg sm:text-base md:text-base lg:text-lg font-bold text-left pl-2 py-1 font-Inter">
                 Log in to your account
             </h3>
@@ -28,15 +33,19 @@
                 placeholder=""
                 class="border rounded-3xl my-2 w-full"
                 />
-                <button class="rounded-3xl my-2 w-full bg-[#d9d9d9] text-lg sm:text-sm md:text-sm lg:text-base font-bold p-2 py-3 font-Inter">
+                <button on:click={log_in} class="rounded-3xl my-2 w-full bg-[#d9d9d9] text-lg sm:text-sm md:text-sm lg:text-base font-bold p-2 py-3 font-Inter">
                     Sign in to your account
                 </button>
-            <img src="/images/or_line.svg" alt="orl" class="w-16 sm:w-25 md:w-50 lg:w-100 h-auto mx-auto"/>
-            <img src="/images/google_log.svg" alt="orl" class="w-16 sm:w-25 md:w-50 lg:w-100 h-auto mx-auto py-2"/>
+            <img src="/images/or_line.svg" alt="orl" class="w-16 sm:w-25 md:w-50 lg:w-80 h-auto mx-auto"/>
+            <img src="/images/google_log.svg" alt="orl" class="w-16 sm:w-25 md:w-50 lg:w-80 h-auto mx-auto py-2"/>
             <p class="text-xs sm:text-xs md:text-xs lg:text-xs text-center pl-2 font-Inter">
                 Don't have an account yet? <a href="/sign_up" class="underline">Create an account</a>
             </p>
         </div>
-        
+        <div id="notice" class="rounded-3xl bg-[#FF9595] w-150 p-3 mt-5">
+            <p class="text-xs sm:text-xs md:text-xs lg:text-xs text-center font-bold pl-2 font-Inter">
+                Account expired or account does not exist
+            </p>
+        </div>
     </div>
 </section>
--- cos-app/src/routes/sign_up/+page.svelte
+++ cos-app/src/routes/sign_up/+page.svelte
@@ -1,12 +1,17 @@
 <script>
-  let username = "";
-  let password = "";
+    let username = "";
+    let password = "";
+    import { goto } from '$app/navigation';
+
+    function authenticate() {
+        goto('/two_factor_auth');
+    }
 </script>
 
-<section id="main" class="flex items-center justify-center min-h-screen">
-    <div class="flex flex-col items-center w-16 sm:w-25 md:w-50 lg:w-100 h-auto mx-auto">
-        <img src="/images/logo.svg" alt="logo" class="w-16 sm:w-25 md:w-50 lg:w-100 h-auto mx-auto"/>
-        <div id="log-in" class="py-5 px-2 w-100">
+<section id="main" class="flex flex-col items-center justify-center mt-20">
+    <img src="/images/logo.svg" alt="logo" class="w-16 sm:w-25 md:w-50 lg:w-80 h-auto mx-auto mt-25"/>
+    <div class="flex flex-col items-center w-16 sm:w-25 md:w-50 lg:w-80 h-auto mx-auto">
+        <div id="log-in" class="py-5 px-2 w-80">
             <h3 class="text-lg sm:text-base md:text-base lg:text-lg font-bold text-left pl-2 py-1 font-Inter">
                 Create an account
             </h3>
@@ -37,11 +42,18 @@
                 placeholder=""
                 class="border rounded-3xl my-2 w-full"
                 />
-            <button class="rounded-3xl my-2 w-full bg-[#d9d9d9] text-lg sm:text-sm md:text-sm lg:text-base font-bold p-2 py-3 font-Inter">
+            <button on:click={authenticate}
+                class="rounded-3xl my-2 w-full bg-[#d9d9d9] text-lg sm:text-sm md:text-sm lg:text-base font-bold p-2 py-3 font-Inter">
                 Sign Up
             </button>
             <p class="text-xs sm:text-xs md:text-xs lg:text-xs text-center pl-2 font-Inter">
                 Have an account? <a href="/" class="underline">Log in</a>
+            </p>
+        </div>
+        <div id="notice" class="flex justify-center rounded-xl bg-[#d9d9d9] w-120 p-3 mt-5">
+            <img src="/images/excla.svg" alt="logo" class="w-[0.5] sm:w-1 md:w-2 lg:w-2 h-auto"/>
+            <p class="text-xs sm:text-xs md:text-xs lg:text-xs text-center pl-2 font-Inter">
+                <b>Notice: </b> After sign-up, the account will remain active for five (5) days only.
             </p>
         </div>
         
